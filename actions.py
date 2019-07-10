d6636f81495cad4733572e575ba209a853d3ca84
wegamekinglc <scrappedprince.li@gmail.com>
2019-07-11 00:32:16 +0800

    cont development

--- actions.py
+++ actions.py
@@ -4,25 +4,81 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
+from pathlib import Path
+import pandas as pd
+import numpy as np
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+from bert_serving.client import ConcurrentBertClient
+
+bc = ConcurrentBertClient()
+
+def r_square(src, dst):
+    error = src - dst
+    return 1. - error @ error / (src @ src)
+
+parent_path = Path(__file__).parents[0]
+faq = pd.read_csv(parent_path / "data/faq.csv", index_col=0)
+category = pd.read_csv(parent_path / "data/category.csv", index_col=0)
+embed = pd.read_csv(parent_path / "data/embed.csv", index_col=0)
+
+def convert(x): 
+    x = x[2:-2] 
+    x = x.split(' ') 
+    x = [float(s.strip()) for s in x if s] 
+    return np.array(x) 
 
 
-class ActionAnswerResp(Action):
+embed['embed'] = embed['embed'].apply(convert)
+
+
+class ActionSearchQuestion(Action):
 
     def name(self) -> Text:
-        return "answer_resp"
+        return "search_question"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        last_message = tracker.latest_message['text']
-        dispatcher.utter_message(last_message)
+        question = tracker.latest_message['text']
+        q_vec = bc.encode([question])[0]
 
-        return []
+        faq_copy = faq.copy()
+        faq_copy.loc[:, 'score'] = [r_square(q_vec, row) for row in embed['embed'].values]
+        faq_copy = faq_copy.sort_values('score', ascending=False)
+        exact_answer = faq_copy.iloc[0]['answer']
+
+        if faq_copy.iloc[0]['score'] > 0.9:
+            return [SlotSet("match_status", "exact"), SlotSet("exact_answer", exact_answer)]
+        else:
+            return [SlotSet("match_status", "not")]
+
+class ActionExactResp(Action):
+
+    def name(self) -> Text:
+        return "exact_resp"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(tracker.get_slot('exact_answer'))
+        return [SlotSet("match_status", None), SlotSet("exact_answer", None)]
+
+
+class ActionNotKnown(Action):
+
+    def name(self) -> Text:
+        return "not_known"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message("这个问题我不知道。。。")
+
+        return [SlotSet("match_status", None), SlotSet("exact_answer", None)]
+
--- data/stories.md
+++ data/stories.md
@@ -2,10 +2,23 @@
 * greet
   - utter_greet
 
-## ask path
+## good ask path
 * ask_question
-  - answer_resp
+  - search_question
+  - slot{"match_status": "exact"}
+  - exact_resp
+  - slot{"match_status": null}
+
+## bad ask path
+* ask_question
+  - search_question
+  - slot{"match_status": "not"}
+  - not_known
 
 ## goodbye path
 * goodbye
   - utter_goodbye
+
+## fallback story
+* not_understand
+  - action_default_fallback
